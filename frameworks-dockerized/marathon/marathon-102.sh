61ed54e8a65801ace491fcb3c6f566717eadd4e6
Michał Żyłowski <zylowskimichal@gmail.com>
2016-11-28 15:48:20 +0100

    Dockerized, clusterized marathon

---
+++ frameworks-dockerized/marathon/marathon-102.sh
@@ -0,0 +1,7 @@
+docker run -d \
+-e MARATHON_HOSTNAME=gklab-97a-102 \
+-e MARATHON_HTTPS_ADDRESS=10.91.97.102 \
+-e MARATHON_HTTP_ADDRESS=10.91.97.102 \
+-e MARATHON_MASTER=zk://gklab-97a-101:2181,gklab-97a-102:2181,gklab-97a-103:2181/mesos \
+-e MARATHON_ZK=zk://gklab-97a-101:2181,gklab-97a-102:2181,gklab-97a-103:2181/marathon \
+--name marathon --net host --restart always mesoscloud/marathon:1.1.1-centos-7
