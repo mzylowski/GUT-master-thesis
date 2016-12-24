c567ba12f72c6579cf21bf487ecc6cfa5d9d88d2
Michał Żyłowski <michal.zylowski@intel.com>
2016-12-24 21:13:58 +0100

    Cleaning logs between services reloading

--- configs/gklab-97a-102/reload-node.sh
+++ configs/gklab-97a-102/reload-node.sh
@@ -8,6 +8,10 @@ systemctl stop mesos-slave
 echo "Ensure all services are down, and all containers are removed"
 docker rm -f mesos-slave
 
+echo "Cleaning logs"
+rm -rf /mesos-dockerized/mesos-slave/log/*
+rm -rf /mesos-dockerized/mesos-slave/tmp/*
+
 echo "Applaying new configs"
 cp mesos-slave.service /etc/systemd/system/mesos-slave.service
 
