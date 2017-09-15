2486a5b889697aea6f87e0931f471f154f42c384
Michal Zylowski <michal.zylowski@intel.com>
2017-09-15 16:51:21 +0200

    Simple script for instaling latex deps
    Signed-off-by: mzylowski <zylowskimichal@gmail.com>

---
+++ papers/install_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt install rubber texlive-math-extra texlive-latex-extra-doc texlive-lang-polish texlive-fonts-recommende texlive-latex-recommended texlive-pictures texlive-latex-extrad
